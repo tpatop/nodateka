6dc5a8e5e8173260223525c96d29972397e79700
Tpatop <129570296+tpatop@users.noreply.github.com>
2024-10-23 19:10:32 +0200

    Update unichain.sh

--- unichain.sh
+++ unichain.sh
@@ -1,59 +1,12 @@
 #!/bin/bash
 
-# Цвета
-TERRACOTTA='\033[38;5;208m'
-NC='\033[0m'
+# Вызов скрипта для вывода имени
+bash <(curl -s https://raw.githubusercontent.com/tpatop/nodateka/main/name.sh)
 
-# Функция для вывода терракотового текста
-function show() {
-    echo -e "${TERRACOTTA}$1${NC}"
-}
+# Вызов скрипта для проверки и установки Docker и Docker Compose
+bash <(curl -s https://raw.githubusercontent.com/tpatop/nodateka/main/docker.sh)
 
-# Вывод ASCII-арта
-echo "----------------------------------------------------------------------"
-show '███╗   ██╗ ██████╗ ██████╗  █████╗ ████████╗███████╗██╗  ██╗ █████╗ '
-show '████╗  ██║██╔═══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║ ██╔╝██╔══██╗'
-show '██╔██╗ ██║██║   ██║██║  ██║███████║   ██║   █████╗  █████╔╝ ███████║'
-show '██║╚██╗██║██║   ██║██║  ██║██╔══██║   ██║   ██╔══╝  ██╔═██╗ ██╔══██║'
-show '██║ ╚████║╚██████╔╝██████╔╝██║  ██║   ██║   ███████╗██║  ██╗██║  ██║'
-show '╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝'
-echo "----------------------------------------------------------------------"
-
-# Ожидание 3 секунды перед началом процесса
-sleep 3
-
-# Функция для установки Docker
-install_docker() {
-  echo "Docker не найден. Устанавливаю Docker..."
-  sudo apt update
-  sudo apt install -y docker.io
-  sudo systemctl start docker
-  sudo systemctl enable docker
-  echo "Docker успешно установлен."
-}
-
-# Функция для установки Docker Compose
-install_docker_compose() {
-  echo "Docker Compose не найден. Устанавливаю Docker Compose..."
-  sudo apt-get update
-  sudo apt-get install -y docker-compose-plugin
-  echo "Docker Compose успешно установлен."
-}
-
-# Проверка наличия Docker
-if ! command -v docker &> /dev/null; then
-  install_docker
-else
-  echo "Docker уже установлен."
-fi
-
-# Проверка наличия Docker Compose
-if ! command -v docker compose &> /dev/null; then
-  install_docker_compose
-else
-  echo "Docker Compose уже установлен."
-fi
-
+# Клонирование репозитория Uniswap unichain-node
 echo "Клонирование репозитория Uniswap unichain-node..."
 git clone https://github.com/Uniswap/unichain-node
 
@@ -76,6 +29,7 @@ else
   echo "Ошибка: файл .env.sepolia не найден!"
   exit 1
 fi
+
 # Проверяем, существует ли файл docker-compose.yml
 if [ -f "docker-compose.yml" ]; then
   echo "Файл docker-compose.yml найден. Замена портов..."
@@ -87,7 +41,6 @@ else
   echo "Ошибка: файл docker-compose.yml не найден!"
   exit 1
 fi
-
 
 echo "Запуск Docker Compose..."
 docker compose up -d
