db99d6a0b40f7c9068d7049710c8e6693d278c17
Tpatop <129570296+tpatop@users.noreply.github.com>
2024-11-02 21:23:07 +0100

    Update ritual.sh

--- ritual.sh
+++ ritual.sh
@@ -63,33 +63,44 @@ start_screen_session() {
 configure_files() {
     if confirm "Настроить файлы конфигурации?"; then
         echo "Настройка файлов конфигурации..."
+
+        # Параметры с пользовательским вводом
+        read -p "Введите ваш private_key: " PRIVATE_KEY
+        read -p "Введите значение sleep (по умолчанию 3): " SLEEP
+        SLEEP=${SLEEP:-3}  # Устанавливаем значение по умолчанию
+        read -p "Введите значение batch_size (по умолчанию 1800): " BATCH_SIZE
+        BATCH_SIZE=${BATCH_SIZE:-1800}  # Устанавливаем значение по умолчанию
+        read -p "Введите значение starting_sub_id (по умолчанию 170000): " STARTING_SUB_ID
+        STARTING_SUB_ID=${STARTING_SUB_ID:-170000}  # Устанавливаем значение по умолчанию
+
         CONFIG_PATH="/root/infernet-container-starter/deploy/config.json"
         HELLO_CONFIG_PATH="/root/infernet-container-starter/projects/hello-world/container/config.json"
         DEPLOY_SCRIPT_PATH="/root/infernet-container-starter/projects/hello-world/contracts/script/Deploy.s.sol"
         MAKEFILE_PATH="/root/infernet-container-starter/projects/hello-world/contracts/Makefile"
         DOCKER_COMPOSE_PATH="/root/infernet-container-starter/deploy/docker-compose.yaml"
 
-        sed -i 's|\"rpc_url\":.*|\"rpc_url\": \"https://mainnet.base.org/\",|' "$CONFIG_PATH"
-        sed -i 's|\"registry_address\":.*|\"registry_address\": \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",|' "$CONFIG_PATH"
-        sed -i 's|\"private_key\":.*|\"private_key\": \"<YOUR_PRIVATE_KEY>\",|' "$CONFIG_PATH"
-        sed -i 's|\"sleep\":.*|\"sleep\": 3,|' "$CONFIG_PATH"
-        sed -i 's|\"batch_size\":.*|\"batch_size\": 1800,|' "$CONFIG_PATH"
-        sed -i '/\"batch_size\":/a \ \ \"starting_sub_id\": 170000' "$CONFIG_PATH"
+        sed -i "s|\"rpc_url\":.*|\"rpc_url\": \"https://mainnet.base.org/\",|" "$CONFIG_PATH"
+        sed -i "s|\"registry_address\":.*|\"registry_address\": \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",|" "$CONFIG_PATH"
+        sed -i "s|\"private_key\":.*|\"private_key\": \"$PRIVATE_KEY\",|" "$CONFIG_PATH"
+        sed -i "s|\"sleep\":.*|\"sleep\": $SLEEP,|" "$CONFIG_PATH"
+        sed -i "s|\"batch_size\":.*|\"batch_size\": $BATCH_SIZE,|" "$CONFIG_PATH"
+        sed -i "/\"batch_size\":/a \ \ \"starting_sub_id\": $STARTING_SUB_ID" "$CONFIG_PATH"
+
+        # Изменение порта в docker-compose.yaml
+        sed -i 's|4000:|5000:|' "$DOCKER_COMPOSE_PATH"
 
         cp "$CONFIG_PATH" "$HELLO_CONFIG_PATH"
-        sed -i 's|address registry =.*|address registry = 0x3B1554f346DFe5c482Bb4BA31b880c1C18412170;|' "$DEPLOY_SCRIPT_PATH"
+        sed -i "s|address registry =.*|address registry = 0x3B1554f346DFe5c482Bb4BA31b880c1C18412170;|" "$DEPLOY_SCRIPT_PATH"
 
-        sed -i 's|sender :=.*|sender := <YOUR_PRIVATE_KEY>|' "$MAKEFILE_PATH"
-        sed -i 's|RPC_URL :=.*|RPC_URL := https://mainnet.base.org|' "$MAKEFILE_PATH"
+        sed -i "s|sender :=.*|sender := $PRIVATE_KEY|" "$MAKEFILE_PATH"
+        sed -i "s|RPC_URL :=.*|RPC_URL := https://mainnet.base.org|" "$MAKEFILE_PATH"
 
-        sed -i 's|ritualnetwork/infernet-node:1.3.1|ritualnetwork/infernet-node:1.4.0|' "$DOCKER_COMPOSE_PATH"
-        
-        # Изменение порта 4000 на 5000
-        sed -i 's|4000|5000|g' "$DOCKER_COMPOSE_PATH"
+        sed -i "s|ritualnetwork/infernet-node:1.3.1|ritualnetwork/infernet-node:1.4.0|" "$DOCKER_COMPOSE_PATH"
     else
         echo "Пропущена настройка файлов конфигурации."
     fi
 }
+
 
 # Функция для перезапуска Docker контейнеров
 restart_docker_containers() {
