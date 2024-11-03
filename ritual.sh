7ebac3b60739321508f2a019a4a9f7e5e58ee6ab
Tpatop <129570296+tpatop@users.noreply.github.com>
2024-11-03 23:55:54 +0100

    Update ritual.sh

--- ritual.sh
+++ ritual.sh
@@ -1,101 +1,183 @@
 #!/bin/bash
 
+# Вызов скрипта для вывода имени
+bash <(curl -s https://raw.githubusercontent.com/tpatop/nodateka/main/name.sh)
+
 # Переменные для путей
 CONFIG_PATH="/root/infernet-container-starter/deploy/config.json"
 HELLO_CONFIG_PATH="/root/infernet-container-starter/projects/hello-world/container/config.json"
 DEPLOY_SCRIPT_PATH="/root/infernet-container-starter/projects/hello-world/contracts/script/Deploy.s.sol"
 MAKEFILE_PATH="/root/infernet-container-starter/projects/hello-world/contracts/Makefile"
 DOCKER_COMPOSE_PATH="/root/infernet-container-starter/deploy/docker-compose.yaml"
-FORGE_PATH="$HOME/.foundry/bin/forge"
+#foundryup="/root/.foundry/bin/foundryup"
+#FORGE_PATH="/root/.foundry/bin/forge"
+export PATH=$PATH:/root/.foundry/bin
 
-# Вызов скрипта для вывода имени
-bash <(curl -s https://raw.githubusercontent.com/tpatop/nodateka/main/name.sh)
-
-# Функция подтверждения
+# Функция для запроса подтверждения
 confirm() {
-    read -p "$1 (Enter для продолжения или n для пропуска): " choice
-    [[ -z "$choice" || "$choice" == "y" ]]
+    local prompt="$1"
+    read -p "$prompt [y/n]: " choice
+    if [[ -z "$choice" || "$choice" == "y" ]]; then
+        return 0  # Выполнить действие
+    else
+        return 1  # Пропустить действие
+    fi
 }
 
-# Установка зависимостей
+# Функция для установки зависимостей
 install_dependencies() {
-    echo "Обновление пакетов и установка зависимостей..."
-    sudo apt-get update -y && sudo apt upgrade -y
-    sudo apt-get install -y make build-essential unzip lz4 gcc git jq ncdu tmux cmake clang pkg-config \
-    libssl-dev python3-pip protobuf-compiler bc curl
+    if confirm "Установить необходимые пакеты и зависимости?"; then
+        echo "Обновление пакетов и установка зависимостей..."
+        sudo apt-get update -y && sudo apt upgrade -y
+        sudo apt-get install -y make build-essential unzip lz4 gcc git jq ncdu tmux \
+        cmake clang pkg-config libssl-dev python3-pip protobuf-compiler bc curl
+        echo "Установка Docker и Docker Compose..."
+        bash <(curl -s https://raw.githubusercontent.com/tpatop/nodateka/main/docker.sh)
+        echo "Скачивание необходимого образа"
+        docker pull ritualnetwork/hello-world-infernet:latest
+    else
+        echo "Пропущена установка зависимостей."
+    fi
 }
 
-install_docker() {
-    echo "Установка Docker и Docker Compose..."
-    bash <(curl -s https://raw.githubusercontent.com/tpatop/nodateka/main/docker.sh)
-}
-
+# Функция для клонирования репозитория
 clone_repository() {
-    echo "Клонирование репозитория infernet-container-starter..."
-    git clone https://github.com/ritual-net/infernet-container-starter || { echo "Ошибка клонирования"; exit 1; }
+    local repo_url="https://github.com/ritual-net/infernet-container-starter"
+    local destination="infernet-container-starter"
+    
+    # Запрос у пользователя на клонирование
+    read -p "Скачать репозиторий infernet-container-starter? [y/n]: " confirm
+    confirm=${confirm:-y}
+
+    if [[ "$confirm" == "y" ]]; then
+        # Проверяем, существует ли папка и не является ли она пустой
+        if [[ -d "$destination" && ! -z "$(ls -A $destination)" ]]; then
+            echo "ВНИМАНИЕ: Каталог '$destination' уже существует и не пуст. Клонирование не будет выполнено."
+            read -p "Хотите удалить существующий каталог и клонировать заново? [y/n]: " delete_confirm
+
+            if [[ "$delete_confirm" == "y" ]]; then
+                echo "Удаление существующего каталога и клонирование..."
+                rm -rf "$destination"
+                git clone "$repo_url" "$destination"
+            else
+                echo "Клонирование пропущено."
+            fi
+        else
+            echo "Клонирование репозитория infernet-container-starter..."
+            git clone "$repo_url" "$destination"
+        fi
+    else
+        echo "Клонирование пропущено."
+    fi
     cd infernet-container-starter || exit
 }
 
-# Запуск screen сессии
-start_screen_session() {
-    # Изменение порта в docker-compose.yaml
-    sed -i 's|4000:|5000:|' "$DOCKER_COMPOSE_PATH"
-    sed -i 's|8545:|4999:|' "$DOCKER_COMPOSE_PATH"
-    sed -i "s|ritualnetwork/infernet-node:1.3.1|ritualnetwork/infernet-node:1.4.0|" "$DOCKER_COMPOSE_PATH"
-    echo "Запуск screen сессии 'ritual'..."
-    screen -S ritual -d -m bash -c "project=hello-world make deploy-container"
-    echo "Открыто новое окно screen."
-    sleep 5
-}
-
-# Настройка конфигурационных файлов
+# Функция для настройки конфигурационных файлов
 configure_files() {
-    echo "Настройка файлов конфигурации..."
-    read -p "Введите ваш private_key: " PRIVATE_KEY
-    read -p "Введите значение sleep (по умолчанию 3): " SLEEP
-    SLEEP=${SLEEP:-3}
-    read -p "Введите значение batch_size (по умолчанию 1800): " BATCH_SIZE
-    BATCH_SIZE=${BATCH_SIZE:-1800}
-    read -p "Введите значение starting_sub_id (по умолчанию 170000): " STARTING_SUB_ID
-    STARTING_SUB_ID=${STARTING_SUB_ID:-170000}
-    read -p "Введите адрес RPC (по умолчанию https://mainnet.base.org): " RPC_URL
-    RPC_URL=${RPC_URL:-https://mainnet.base.org}  # Устанавливаем значение по умолчанию
+    if confirm "Настроить файлы конфигурации?"; then
+        echo "Настройка файлов конфигурации..."
 
-    sed -i "s|\"rpc_url\":.*|\"rpc_url\": \"$RPC_URL\",|" "$CONFIG_PATH"
-    sed -i "s|\"registry_address\":.*|\"registry_address\": \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",|" "$CONFIG_PATH"
-    sed -i "s|\"private_key\":.*|\"private_key\": \"$PRIVATE_KEY\",|" "$CONFIG_PATH"
-    sed -i "s|\"sleep\":.*|\"sleep\": $SLEEP,|" "$CONFIG_PATH"
-    sed -i "s|\"batch_size\":.*|\"batch_size\": $BATCH_SIZE,|" "$CONFIG_PATH"
-    sed -i "/\"batch_size\":/a \ \ \"starting_sub_id\": $STARTING_SUB_ID" "$CONFIG_PATH"
-    cp "$CONFIG_PATH" "$HELLO_CONFIG_PATH"
+        # Параметры с пользовательским вводом
+        read -p "Введите ваш private_key: " PRIVATE_KEY
+        read -p "Введите значение sleep [3]: " SLEEP
+        SLEEP=${SLEEP:-3}
+        read -p "Введите значение batch_size [1800]: " BATCH_SIZE
+        BATCH_SIZE=${BATCH_SIZE:-1800}
+        read -p "Введите значение starting_sub_id [180000]: " STARTING_SUB_ID
+        STARTING_SUB_ID=${STARTING_SUB_ID:-180000}
+        read -p "Введите адрес RPC [https://mainnet.base.org]: " RPC_URL
+        RPC_URL=${RPC_URL:-https://mainnet.base.org}
 
-    sed -i "s|address registry =.*|address registry = 0x3B1554f346DFe5c482Bb4BA31b880c1C18412170;|" "$DEPLOY_SCRIPT_PATH"
-    sed -i "s|sender :=.*|sender := $PRIVATE_KEY|" "$MAKEFILE_PATH"
-}
+        # Резервное копирование файлов
+        cp "$HELLO_CONFIG_PATH" "${HELLO_CONFIG_PATH}.bak"
+        cp "$DEPLOY_SCRIPT_PATH" "${DEPLOY_SCRIPT_PATH}.bak"
+        cp "$MAKEFILE_PATH" "${MAKEFILE_PATH}.bak"
+        cp "$DOCKER_COMPOSE_PATH" "${DOCKER_COMPOSE_PATH}.bak"
 
-restart_docker_containers() {
-    echo "Перезапуск Docker контейнеров..."
-    docker pull ritualnetwork/hello-world-infernet
-    docker compose -f "$DOCKER_COMPOSE_PATH" down
-    docker compose -f "$DOCKER_COMPOSE_PATH" up -d
-}
+        # Изменения в файле конфигурации
+        sed -i 's|4000,|5000,|' "$HELLO_CONFIG_PATH"
+        sed -i 's|"3000"|"4998"|' "$HELLO_CONFIG_PATH"
+        sed -i 's|:3000|:4998|' "$HELLO_CONFIG_PATH"
+        sed -i "s|\"rpc_url\":.*|\"rpc_url\": \"$RPC_URL\",|" "$HELLO_CONFIG_PATH"
+        sed -i "s|\"registry_address\":.*|\"registry_address\": \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",|" "$HELLO_CONFIG_PATH"
+        sed -i "s|\"private_key\":.*|\"private_key\": \"$PRIVATE_KEY\",|" "$HELLO_CONFIG_PATH"
+        sed -i "s|\"sleep\":.*|\"sleep\": $SLEEP,|" "$HELLO_CONFIG_PATH"
+        sed -i "s|\"batch_size\":.*|\"batch_size\": $BATCH_SIZE,|" "$HELLO_CONFIG_PATH"
+        sed -i "s|\"starting_sub_id\":.*|\"starting_sub_id\": $STARTING_SUB_ID,|" "$HELLO_CONFIG_PATH"
 
-install_foundry() {
-    echo "Установка Foundry..."
-    curl -L https://foundry.paradigm.xyz | bash
-    source ~/.bashrc
-    "$FORGE_PATH" || echo "Foundry не найден."
-}
+        # Изменения в deploy-скрипте и Makefile
+        sed -i "s|address registry =.*|address registry = 0x3B1554f346DFe5c482Bb4BA31b880c1C18412170;|" "$DEPLOY_SCRIPT_PATH"
+        sed -i "s|sender :=.*|sender := $PRIVATE_KEY|" "$MAKEFILE_PATH"
+        sed -i "s|RPC_URL :=.*|RPC_URL := $RPC_URL|" "$MAKEFILE_PATH"
 
-install_project_dependencies() {
-    echo "Установка зависимостей для hello-world проекта..."
-    if [ ! -f "$FORGE_PATH" ]; then
-        echo "forge не найден в $FORGE_PATH. Убедитесь, что Foundry установлен."
-        return 1
+        # Изменение порта в docker-compose.yaml
+        sed -i 's|4000:|5000:|' "$DOCKER_COMPOSE_PATH"
+        sed -i 's|8545:|4999:|' "$DOCKER_COMPOSE_PATH"
+        sed -i "s|ritualnetwork/infernet-node:1.3.1|ritualnetwork/infernet-node:1.4.0|" "$DOCKER_COMPOSE_PATH"    
+
+        echo "Настройка файлов завершена."
+    else
+        echo "Пропущена настройка файлов конфигурации."
     fi
-    cd /root/infernet-container-starter/projects/hello-world/contracts || exit
-    "$FORGE_PATH" install --no-commit foundry-rs/forge-std || rm -rf lib/forge-std && "$FORGE_PATH" install --no-commit foundry-rs/forge-std
-    "$FORGE_PATH" install --no-commit ritual-net/infernet-sdk || rm -rf lib/infernet-sdk && "$FORGE_PATH" install --no-commit ritual-net/infernet-sdk
+}
+
+# Функция для запуска screen сессии
+start_screen_session() {
+    if confirm "Запустить screen сессию 'ritual'?"; then
+        echo "Запуск screen сессии 'ritual'..."
+        screen -S ritual -d -m bash -c "project=hello-world make deploy-container; bash"
+        echo "Открыто новое окно screen."
+    else
+        echo "Пропущен запуск screen сессии."
+    fi
+}
+
+# Функция для перезапуска Docker контейнеров
+restart_docker_containers() {
+    if confirm "Перезапустить Docker контейнеры?"; then
+        echo "Перезапуск Docker контейнеров..."
+        cd /root && docker compose -f $DOCKER_COMPOSE_PATH down
+        docker compose -f $DOCKER_COMPOSE_PATH up -d
+    else
+        echo "Пропущен перезапуск Docker контейнеров."
+    fi
+}
+
+# Функция для проверки и выполнения foundryup
+run_foundryup() {
+    # Проверяем, добавлен ли путь до Foundry в .bashrc
+    if grep -q 'foundry' ~/.bashrc; then
+        source ~/.bashrc
+        echo "Запускаем foundryup..."
+        foundryup
+    else
+        echo "Путь до foundryup не найден в .bashrc."
+        echo "Пожалуйста, выполните 'source ~/.bashrc' вручную или перезапустите терминал."
+    fi
+}
+
+# Функция для установки Foundry
+install_foundry() {
+    if confirm "Установить Foundry?"; then
+        echo "Установка Foundry..."
+        curl -L https://foundry.paradigm.xyz | bash
+        #run_foundryup
+        foundryup
+    else
+        echo "Пропущена установка Foundry."
+    fi
+}
+
+# Функция для установки зависимостей проекта
+install_project_dependencies() {
+    if confirm "Установить зависимости для hello-world проекта?"; then
+        echo "Установка зависимостей для hello-world проекта..."
+        cd /root/infernet-container-starter/projects/hello-world/contracts || exit
+        forge install --no-commit foundry-rs/forge-std || { echo "Ошибка при установке зависимости forge-std. Устраняем..."; rm -rf lib/forge-std && forge install --no-commit foundry-rs/forge-std; }
+        forge install --no-commit ritual-net/infernet-sdk || { echo "Ошибка при установке зависимости infernet-sdk. Устраняем..."; rm -rf lib/infernet-sdk && forge install --no-commit ritual-net/infernet-sdk; }
+    else
+        echo "Пропущена установка зависимостей."
+    fi
 }
 
 # Функция для развертывания контракта
@@ -108,41 +190,75 @@ deploy_contract() {
         echo "Пропущено развертывание контракта."
     fi
 }
-# Запрос
+# Функция для замены адреса контракта
 call_contract() {
-    if confirm "Вызвать новый запрос?"; then
+    if confirm "Вставить Contract Address из предыдущего шага?"; then
         read -p "Введите Contract Address: " CONTRACT_ADDRESS
         echo "Заменяем старый номер в CallsContract.s.sol..."
         sed -i "s|SaysGM(.*)|SaysGM($CONTRACT_ADDRESS)|" ~/infernet-container-starter/projects/hello-world/contracts/script/CallContract.s.sol
         echo "Выполняем команду project=hello-world make call-contract..."
         project=hello-world make call-contract
     else
-        echo "Пропущен вызов запроса."
+        echo "Пропущена вставка Contract Address."
     fi
 }
 
+# Функция для замены RPC URL
 replace_rpc_url() {
     if confirm "Заменить RPC URL?"; then
-        read -p "Введите новый RPC URL (по умолчанию https://mainnet.base.org): " NEW_RPC_URL
-        NEW_RPC_URL=${NEW_RPC_URL:-"https://mainnet.base.org"}
-        CONFIG_PATHS=("$HELLO_CONFIG_PATH" "$CONFIG_PATH" "$MAKEFILE_PATH")
+        read -p "Введите новый RPC URL: " NEW_RPC_URL
+
+        CONFIG_PATHS=(
+            "/root/infernet-container-starter/projects/hello-world/container/config.json"
+            "/root/infernet-container-starter/deploy/config.json"
+            "/root/infernet-container-starter/projects/hello-world/contracts/Makefile"
+        )
+
+        # Переменная для отслеживания найденных файлов
+        files_found=false
+
         for config_path in "${CONFIG_PATHS[@]}"; do
-            [[ -f "$config_path" ]] && sed -i "s|https://mainnet.base.org|$NEW_RPC_URL|g" "$config_path" && echo "Заменен RPC URL в $config_path"
+            if [[ -f "$config_path" ]]; then
+                sed -i "s|https://mainnet.base.org|$NEW_RPC_URL|g" "$config_path"
+                echo "RPC URL заменен в $config_path"
+                files_found=true  # Устанавливаем флаг, если файл найден
+            else
+                echo "Файл $config_path не найден, пропускаем."
+            fi
         done
+
+        # Если не найдено ни одного файла, выводим сообщение
+        if ! $files_found; then
+            echo "Не удалось найти ни одного конфигурационного файла для замены RPC URL."
+            return  # Завершаем выполнение функции
+        fi
         restart_docker_containers
+        echo "Контейнеры перезапущены после замены RPC URL."
+    else
+        echo "Замена RPC URL отменена."
     fi
 }
 
+# Функция для удаления ноды
 delete_node() {
     if confirm "Удалить ноду и очистить файлы?"; then
-        echo "Удаление ноды..."
-        docker compose -f "$DOCKER_COMPOSE_PATH" down
-        rm -rf /root/infernet-container-starter
+        echo "Остановка и удаление контейнеров"
+        cd ~ 
+        docker compose -f $DOCKER_COMPOSE_PATH down
+        echo "Удаление директории проекта"
+        rm -rf infernet-container-starter
+        echo "Удаление образов проекта, хранилищ..."
+        docker system prune -a
+        echo "Нода удалена и файлы очищены."
+    else
+        echo "Удаление ноды отменено."
     fi
 }
 
+# Функция для отображения меню
 show_menu() {
     echo ""
+    echo "Выберите действие:"
     echo "1. Установка ноды"
     echo "2. Логи ноды"
     echo "3. Замена RPC"
@@ -150,31 +266,44 @@ show_menu() {
     echo "0. Выход"
 }
 
+# Функция для обработки выбора пользователя
 handle_choice() {
     case "$1" in
         1)
             echo "Запущена установка ноды..."
             install_dependencies
-            install_docker
             clone_repository
-            start_screen_session
             configure_files
-            restart_docker_containers
+            start_screen_session
             install_foundry
             install_project_dependencies
             deploy_contract
             call_contract
             ;;
-        2) docker logs -f --tail 20 infernet-node ;;
-        3) replace_rpc_url ;;
-        9) delete_node ;;
-        0) exit 0 ;;
-        *) echo "Неверный выбор" ;;
+        2)
+            echo "Отображение логов ноды..."
+            docker logs -f --tail 20 infernet-node
+            ;;
+        3)
+            echo "Замена RPC URL..."
+            replace_rpc_url
+            ;;
+        9)
+            echo "Удаление ноды..."
+            delete_node
+            ;;
+        0)
+            echo "Выход..."
+            exit 0
+            ;;
+        *)
+            echo "Неверный выбор, попробуйте снова."
+            ;;
     esac
 }
 
 while true; do
     show_menu
     read -p "Ваш выбор: " action
-    handle_choice "$action"
+    handle_choice "$action"  # Используем переменную action
 done
