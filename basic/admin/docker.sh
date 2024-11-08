f77f7d0f7d948bb3aea536f09716fbc83d94fff0
tpatop <tpatop2000@gmail.com>
2024-11-08 23:47:42 +0300

    docker

--- basic/admin/docker.sh
+++ basic/admin/docker.sh
@@ -16,6 +16,7 @@ install_docker() {
 # Функция для установки Docker Compose
 install_docker_compose() {
   echo "Docker Compose не найден. Устанавливаю docker-compose и docker compose (plugin)..."
+  
   VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
   curl -L "https://github.com/docker/compose/releases/download/"$VER"/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
