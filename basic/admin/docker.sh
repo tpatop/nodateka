b84acb8d0c1b2861d6eae9f729edde407546e50c
tpatop <tpatop2000@gmail.com>
2024-11-08 23:51:41 +0300

    upd

--- basic/admin/docker.sh
+++ basic/admin/docker.sh
@@ -4,7 +4,7 @@
 install_docker() {
   echo "Docker не найден. Устанавливаю Docker..."
   sudo apt install -y ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gp
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker $USER
@@ -16,15 +16,19 @@ install_docker() {
 # Функция для установки Docker Compose
 install_docker_compose() {
   echo "Docker Compose не найден. Устанавливаю docker-compose и docker compose (plugin)..."
-  
-  VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-  curl -L "https://github.com/docker/compose/releases/download/"$VER"/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
 
+  VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+  
+  # Установка бинарной версии docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/${VER}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  # Установка плагина docker compose
   DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
   mkdir -p $DOCKER_CONFIG/cli-plugins
-  curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+  sudo curl -SL "https://github.com/docker/compose/releases/download/${VER}/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)" -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
   docker-compose version
   docker compose version
   echo "docker-compose и docker compose (plugin) установлены."
