52791dfc5a9fcb05b9fd84902f5d629b45a593f9
tpatop <tpatop2000@gmail.com>
2024-11-08 23:46:03 +0300

    docker

--- basic/admin/docker.sh
+++ basic/admin/docker.sh
@@ -3,19 +3,30 @@
 # Функция для установки Docker
 install_docker() {
   echo "Docker не найден. Устанавливаю Docker..."
-  sudo apt update
-  sudo apt install -y docker.io
-  sudo systemctl start docker
-  sudo systemctl enable docker
+  sudo apt install -y ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gp
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+  newgrp docker
+  docker version
   echo "Docker успешно установлен."
 }
 
 # Функция для установки Docker Compose
 install_docker_compose() {
-  echo "Docker Compose не найден. Устанавливаю Docker Compose..."
-  sudo apt update
-  sudo apt install -y docker-compose-plugin
-  echo "Docker Compose успешно установлен."
+  echo "Docker Compose не найден. Устанавливаю docker-compose и docker compose (plugin)..."
+  VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+  curl -L "https://github.com/docker/compose/releases/download/"$VER"/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+
+  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+  mkdir -p $DOCKER_CONFIG/cli-plugins
+  curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+  docker-compose version
+  docker compose version
+  echo "docker-compose и docker compose (plugin) установлены."
 }
 
 # Функция для проверки и включения Docker демона
