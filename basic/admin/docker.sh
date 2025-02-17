61d4cdfea56a034c3d1b70f53a267561ad945c1b
tpatop <tpatop2000@gmail.com>
2025-02-17 23:19:17 +0300

    upgrade docker

--- basic/admin/docker.sh
+++ basic/admin/docker.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# Функция для установки Docker
-install_docker() {
-  echo "Docker не найден. Устанавливаю Docker..."
-  sudo apt install -y ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Функция для установки или обновления Docker
+install_or_update_docker() {
+  echo "Обновляю список пакетов..."
+  sudo apt update
+
+  if ! command -v docker &> /dev/null; then
+    echo "Docker не найден. Устанавливаю Docker..."
+    sudo apt install -y ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  fi
+  
+  echo "Обновляю Docker до последней стабильной версии..."
   sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker $USER
-  newgrp docker
+#  newgrp docker
   docker version
-  echo "Docker успешно установлен."
+  echo "Docker успешно установлен или обновлен."
 }
 
-# Функция для установки Docker Compose
-install_docker_compose() {
-  echo "Docker Compose не найден. Устанавливаю docker-compose и docker compose (plugin)..."
-
+# Функция для установки или обновления Docker Compose
+install_or_update_docker_compose() {
+  echo "Обновляю Docker Compose..."
   VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
   
   # Установка бинарной версии docker-compose
@@ -31,7 +37,7 @@ install_docker_compose() {
 
   docker-compose version
   docker compose version
-  echo "docker-compose и docker compose (plugin) установлены."
+  echo "Docker Compose успешно установлен или обновлен."
 }
 
 # Функция для проверки и включения Docker демона
@@ -45,18 +51,10 @@ check_docker_daemon() {
   fi
 }
 
-# Проверка наличия Docker
-if ! command -v docker &> /dev/null; then
-  install_docker
-else
-  echo "Docker уже установлен."
-fi
-
-# Проверка наличия Docker Compose
-if ! docker compose version &> /dev/null; then
-  install_docker_compose
-else
-  echo "Docker Compose уже установлен."
-fi
-
+install_or_update_docker
+install_or_update_docker_compose
 check_docker_daemon
+# Перезапуск всех упавших контейнеров
+sleep 2
+docker ps -a --filter "status=exited" --format "{{.ID}}" | xargs -r docker restart
+echo 'Скрипт выполнен!'
